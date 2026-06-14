baab300dd7c21378367afa3325b129f3d91294f3
Kai.Xiong <xiongkai@petalmail.com>
2026-06-14 13:52:52 +0800

    更新 Flutter Rust 桥接库至 2.13.0-beta.1，重构主函数以支持异步初始化，调整依赖版本，优化代码结构

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:rust_ohos_app/api/hello.dart';
+import 'package:rust_ohos_app/api/ffi.dart';
+import 'package:rust_ohos_app/frb_generated.dart' show RustLib;
 import 'dart:async';
 
-import 'package:rust_ohos_app/rust_ohos_app.dart' as rust_ohos_app;
-
-void main() {
-  // rust_ohos_app.
+Future<void> main() async {
+  await RustLib.init();
   runApp(const MyApp());
 }
 
@@ -22,8 +23,12 @@ class _MyAppState extends State<MyApp> {
   @override
   void initState() {
     super.initState();
-    sumResult = rust_ohos_app.sum(1, 2);
-    sumAsyncResult = rust_ohos_app.sumAsync(3, 4);
+    // sumResult = rust_ohos_app.sum(1, 2);
+    // sumAsyncResult = rust_ohos_app.sumAsync(3, 4);
+
+    // final helloResult = hello(hello: 'Hello from Dart!');
+    // debugPrint('helloResult: $helloResult');
+    // rust_ohos_app.
   }
 
   @override
@@ -32,42 +37,56 @@ class _MyAppState extends State<MyApp> {
     const spacerSmall = SizedBox(height: 10);
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: const Text('Native Packages')),
-        body: SingleChildScrollView(
-          child: Container(
-            padding: const .all(10),
-            child: Column(
-              children: [
-                const Text(
-                  'This calls a native function through FFI that is shipped as source in the package. '
-                  'The native code is built as part of the Flutter Runner build.',
-                  style: textStyle,
-                  textAlign: .center,
-                ),
-                spacerSmall,
-                Text(
-                  'sum(1, 2) = $sumResult',
-                  style: textStyle,
-                  textAlign: .center,
-                ),
-                spacerSmall,
-                FutureBuilder<int>(
-                  future: sumAsyncResult,
-                  builder: (BuildContext context, AsyncSnapshot<int> value) {
-                    final displayValue = (value.hasData)
-                        ? value.data
-                        : 'loading';
-                    return Text(
-                      'await sumAsync(3, 4) = $displayValue',
-                      style: textStyle,
-                      textAlign: .center,
-                    );
-                  },
-                ),
-              ],
-            ),
-          ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () async {
+            // setState(() {
+            //   sumResult = rust_ohos_app.sum(sumResult, 1);
+            //   sumAsyncResult = rust_ohos_app.sumAsync(sumResult, 1);
+            // });
+            await hello(hello: 'Hello from Dart!');
+            final sumResult = await sum(a: 1, b: 2);
+            debugPrint('sumResult: $sumResult');
+            final sumAsyncResult = await sumLongRunning(a: 3, b: 4);
+            debugPrint('sumAsyncResult: $sumAsyncResult');
+          },
+          child: const Icon(Icons.add),
         ),
+        appBar: AppBar(title: const Text('Native Packages')),
+        // body: SingleChildScrollView(
+        //   child: Container(
+        //     padding: const .all(10),
+        //     child: Column(
+        //       children: [
+        //         const Text(
+        //           'This calls a native function through FFI that is shipped as source in the package. '
+        //           'The native code is built as part of the Flutter Runner build.',
+        //           style: textStyle,
+        //           textAlign: .center,
+        //         ),
+        //         spacerSmall,
+        //         Text(
+        //           'sum(1, 2) = $sumResult',
+        //           style: textStyle,
+        //           textAlign: .center,
+        //         ),
+        //         spacerSmall,
+        //         FutureBuilder<int>(
+        //           future: sumAsyncResult,
+        //           builder: (BuildContext context, AsyncSnapshot<int> value) {
+        //             final displayValue = (value.hasData)
+        //                 ? value.data
+        //                 : 'loading';
+        //             return Text(
+        //               'await sumAsync(3, 4) = $displayValue',
+        //               style: textStyle,
+        //               textAlign: .center,
+        //             );
+        //           },
+        //         ),
+        //       ],
+        //     ),
+        //   ),
+        // ),
       ),
     );
   }
